isabella232/d-feet

commit e6488e6c0fd0416c54ecff1dd682f0ae8aa4c6fb

Honor theme's foreground color
This makes property names readable on dark themes like adwaita-dark.

src/dfeet/introspection_helper.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from gi.repository import GObject, Gio
+from gi.repository import GObject, Gio, Gtk
 from dfeet import dbus_utils
 
+_style_context = None
+_fg_color = "#000000"
+
+def fg_color():
+    global _style_context, _fg_color
+    if _style_context is None:
+        _style_context = Gtk.StyleContext()
+        color = _style_context.get_color(Gtk.StateFlags.NORMAL)
+        _fg_color = "#%x%x%x" % (color.red * 255, color.green * 255, color.blue * 255)
+    return _fg_color
 
 def args_signature_markup(arg_signature):
     return '<small><span foreground="#2E8B57">%s</span></small>' % (arg_signature)
 
-
 def args_name_markup(arg_name):
-    return '<small><span foreground="#000000">%s</span></small>' % (arg_name)
-
+    return '<small><span foreground="%s">%s</span></small>' % (fg_color(), arg_name)
 
 class DBusNode(GObject.GObject):
     """object to represent a DBus Node (object path)"""

commit 12b8b9970e478ae6abb0fa33ebadacfbc9991b23

Fix coding style errors reported by make check
These were introduced in e6488e6c0fd0416c54ecff1dd682f0ae8aa4c6fb and
e6488e6c0fd0416c54ecff1dd682f0ae8aa4c6fb:

    src/dfeet/introspection_helper.py:9:1: E302 expected 2 blank lines, found 1
    src/dfeet/introspection_helper.py:14:100: E501 line too long (107 > 99 characters)
    src/dfeet/introspection_helper.py:17:1: E302 expected 2 blank lines, found 1
    src/dfeet/introspection_helper.py:20:1: E302 expected 2 blank lines, found 1
    src/dfeet/introspection_helper.py:23:1: E302 expected 2 blank lines, found 1

src/dfeet/introspection_helper.py
@@ -6,19 +6,27 @@ from dfeet import dbus_utils
 _style_context = None
 _fg_color = "#000000"
 
+
 def fg_color():
     global _style_context, _fg_color
     if _style_context is None:
         _style_context = Gtk.StyleContext()
         color = _style_context.get_color(Gtk.StateFlags.NORMAL)
-        _fg_color = "#%02x%02x%02x" % (int(color.red) * 255, int(color.green) * 255, int(color.blue) * 255)
+        _fg_color = "#%02x%02x%02x" % (
+            int(color.red) * 255,
+            int(color.green) * 255,
+            int(color.blue) * 255,
+        )
     return _fg_color
+
 
 def args_signature_markup(arg_signature):
     return '<small><span foreground="#2E8B57">%s</span></small>' % (arg_signature)
 
+
 def args_name_markup(arg_name):
     return '<small><span foreground="%s">%s</span></small>' % (fg_color(), arg_name)
+
 
 class DBusNode(GObject.GObject):
     """object to represent a DBus Node (object path)"""